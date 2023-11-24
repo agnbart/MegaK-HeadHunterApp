552f29a03f7df6c9d32e51f2613cbace90c15348
agnbart <polpumi@gmail.com>
2023-11-24 14:42:41 +0100

    feat: setup tipescript

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+import * as express from 'express';
+export const app = express();
+
+app.use(express.json());
 
 app.get('/', function (req, res, next) {
     res.json({
